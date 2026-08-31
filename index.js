18e073fc3f93b2546230eb3e86fdd19aeac6edcd
owl <tummasrinivas2502@gmail.com>
2026-08-31 19:18:56 +0530

    Created First server on port 5000

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("lol");
+// const http =  require('http'); // commonjs module system
+
+import http from "http"; // es6 module system
+
+const server = http.createServer((req,res)=>{
+  res.end("hello world");
+}) // created server method
+
+server.listen(5000,()=>{
+  console.log("server is listening on port 5000");
+}); // makes this server listen on port 5000 
