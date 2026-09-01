bc17f70787b2f7fb881fd1a92b48a09359cfe14d
owl <tummasrinivas2502@gmail.com>
2026-09-01 17:14:30 +0530

    Understanding Headers ,

--- server/index.js
+++ server/index.js
@@ -10,14 +10,14 @@ app.use(cors({
 })); // Enable CORS for the specified origin
 app.use(express.json()); // Middleware to parse JSON request bodies
 
-let password = "1234"; // Define a password variable
+// let password = "1234"; // Define a password variable
 
-app.use((req,res,next) => {
-  if(req.body.pass != password){
-    res.send("Incorrect password");
-  }
-  next();
-}) // custom middleware function that checks if the password in the request body matches the defined password. If it doesn't match, it sends a response with "Incorrect password". If it matches, it calls next() to proceed to the next middleware or route handler.
+// app.use((req,res,next) => {
+//   if(req.body.pass != password){
+//     res.send("Incorrect password");
+//   }
+//   next();
+// }) // custom middleware function that checks if the password in the request body matches the defined password. If it doesn't match, it sends a response with "Incorrect password". If it matches, it calls next() to proceed to the next middleware or route handler.
 
 
 app.get("/", (req, res) => {
@@ -28,6 +28,14 @@ app.post("/", (req, res) => {
   console.log(req.body); // Log the request body to the console
   res.status(200).send({ success: true });//shows that the request was successful and sends a JSON response back to the client on browser console
 });
+
+app.get("/head",(req,res) =>{
+  console.log(req.headers); // Log the request headers to the console
+  res.set("x-Custom-Header", "This is a custom header"); // Set a custom header in the response
+  res.send("Check the response headers for the custom header"); // Send a response to the client , 
+  // res.send() is imp after setting header , // because if we don't send a response, the client will keep waiting for a response and the request will time out.
+})
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
