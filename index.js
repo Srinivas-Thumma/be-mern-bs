a1b74c71e5c81fd158419e7134ac0ffd6c8d625e
owl <tummasrinivas2502@gmail.com>
2026-09-02 16:51:31 +0530

    Dont know , what changed

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
+import User from "./models/user.model.js";
 
 const app = express();
 const port = 8000;
 
 //mongodb connection url
 const mongoURL = "mongodb+srv://narak:beluga1245@cluster0.e0r9qa0.mongodb.net/Mern";
+
+app.use(express.json());//middleware to parse json data from the request body
 
 //function to connect to MongoDB
 const connectDB = async () => {
@@ -22,6 +25,17 @@ const connectDB = async () => {
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.post("/create",async (req,res)=>{
+  try {
+      let  {name,age,email,password,userName} = req.body;
+      const newUser = User.create({name,age,email,password,userName});
+      return res.status(201).json({message:"User created successfully",user:newUser});
+  }catch (error) { 
+      console.error("Error creating user:", error);
+      return res.status(500).json({message:"Error creating user"});
+  }
+})
 
 app.listen(port, () => {
   //run the connectDB function to establish the connection
