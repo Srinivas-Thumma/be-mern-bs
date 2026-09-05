80ba202a3754abe6d45f8f2bfc2cf3b7b472c819
owl <tummasrinivas2502@gmail.com>
2026-09-05 18:43:00 +0530

    Ran Server

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+let app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
